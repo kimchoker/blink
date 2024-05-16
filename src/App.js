ca0eddb2ab1ae10b74201599e5c127eee93f60c7
kimchoker <kimchoker@naver.com>
2024-05-16 13:26:49 +0900

    골자 작업중

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
 import { Button, Navbar, Container, Nav } from 'react-bootstrap'
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import Main from './pages/Main';
+import { Route, Routes, Link } from 'react-router-dom';
+
+
 function App() {
   return (
    <div>
-    
+    <Routes>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/signup' element={<SignUp/>}/>
+      <Route path='/' element={<Main/>}/>
+    </Routes>
    </div>
   );
 }
